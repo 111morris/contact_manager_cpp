969df50943cb645e99211ef842dbcef0c8ed9837
mulandi <mulandimorris1@gmail.com>
2025-06-01 18:04:26 +0300

    dvpkg

--- main.cpp
+++ main.cpp
@@ -10,5 +10,22 @@ int main() {
   cout<<"\n1. Add contact\n2. Search contact\n3. help\n4. Exit\n\n";
   cin>>choice;
 
+  switch (choice) {
+    case 1:
+    addContact();
+    break;
+    case 2:
+    searchContact();
+    break;
+    case 3:
+    help();
+    break;
+    case 4:
+    self_exit();
+    break;
+    default:
+    cout<<"\nInvalid input!";
+    cout<<"\nPress any key to continue..";
+  }
   return(0);
 }
