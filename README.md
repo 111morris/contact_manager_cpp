a13063669908ac57832ed8560a3bb7a8dd013ad5
mulandi <mulandimorris1@gmail.com>
2025-06-01 18:29:54 +0300

    dvpkg

---
+++ README.md
@@ -0,0 +1,1 @@
+# snake game built in c++
--- main.cpp
+++ main.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
-#include <conio.h>
-using namespace;
-
+using namespace std;
 int main() {
   short int choice;
-  system("cls");
-  system("clear");
-  cout<<"\nContact Manager";
-  cout<<"\n1. Add contact\n2. Search contact\n3. help\n4. Exit\n\n";
-  cin>>choice;
 
-  switch (choice) {
-    case 1:
-    addContact();
-    break;
-    case 2:
-    searchContact();
-    break;
-    case 3:
-    help();
-    break;
-    case 4:
-    self_exit();
-    break;
-    default:
-    cout<<"\nInvalid input!";
+  while (true) {
+    #ifdef _WIN32
+      system(cls);
+    #else
+      system(clear);
+    #endif
 
+    cout << nContact Manager;
+    cout << n1. Add contactn2. Search contactn3. Helpn4. Exitnn;
+    cout << Enter choice: ;
+    cin >> choice;
+
+    switch (choice) {
+      case 1: addContact(); break;
+      case 2: searchContact(); break;
+      case 3: help(); break;
+      case 4: self_exit(); return 0;
+    }
+
+    cout << nPress Enter to continue...;
+    cin.ignore(); // Clear leftover newline
+    cin.get();    // Wait for user input
   }
-  return(0);
 }
