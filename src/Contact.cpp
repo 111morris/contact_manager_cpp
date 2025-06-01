bb0c067615ff4f956c04424aa8b0279b1663e4f4
mulandi <mulandimorris1@gmail.com>
2025-06-02 00:22:06 +0300

    updated the Contact .cpp file

---
+++ src/Contact.cpp
@@ -0,0 +1,17 @@
+#include "Contact.hpp"
+#include <iostream>
+
+Contact::Contact(std::string name, std::string phone, std::string email)
+: name(name), phone(phone), email(email){}
+
+void Contact::display() const{
+  std::cout << "Name: "<<name <<"\nPhone: "<< "\nEmail: "<< email << "\n";
+}
+
+nlohmann::json Contact::toJson() const {
+  return nlohmann::json{{"name", name}, {"phone", phone}, {"email", email}};
+}
+
+Contact Contact::fromJson(const nlohmann::json& j) {
+  return Contact(j.at("name"), j.at("phone"), j.at("email"));
+}
