d6ec78df6ea29e521e5760ee9e6326230f59e7f6
mulandi <mulandimorris1@gmail.com>
2025-06-01 18:00:23 +0300

    first commit with the new project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace;
+
+int main() {
+  short int choice;
+  system("cls");
+  system("clear");
+  cout<<"\nContact Manager";
+  cout<<"\n1. Add contact\n2. Search contact\n3. help\n4. Exit\n\n";
+  cin>>choice;
+
+  return(0);
+}
