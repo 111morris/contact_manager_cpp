f3502b82f6c8cd8e0e097b192715ba69418a479d
mulandi <mulandimorris1@gmail.com>
2025-06-02 13:55:48 +0300

    adding the contact.hpp file

--- src/Contact.hpp
+++ src/Contact.hpp
@@ -3,16 +3,18 @@
 #include <nlohmann/json.hpp>
 
 class Contact {
-public: 
-  std::string name;
-  std::string phone;
-  std::string email;
+public:
+    std::string name;
+    std::string phone;
+    std::string email;
 
-  Contact() = default;
-  Contact(std::string name, std::string phone, std::string email);
+    Contact() = default;
+    Contact(std::string name, std::string phone, std::string email);
 
-  void display() const;
+    void display() const;
 
-  nlohmann::json to json() const;
-  static Contact fromJson(const nlohmann::json& j);
+    // Serialization
+    nlohmann::json toJson() const;
+    static Contact fromJson(const nlohmann::json& j);
 };
+
