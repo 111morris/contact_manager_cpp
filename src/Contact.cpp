6ed0324915782b158a2564976e07e3d85d2613e3
mulandi <mulandimorris1@gmail.com>
2025-06-02 13:56:34 +0300

    adding contact.cpp file and updating

--- src/Contact.cpp
+++ src/Contact.cpp
@@ -1,17 +1,19 @@
 #include "Contact.hpp"
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 Contact::Contact(std::string name, std::string phone, std::string email)
-: name(name), phone(phone), email(email){}
+    : name(name), phone(phone), email(email) {}
 
-void Contact::display() const{
-  std::cout << "Name: "<<name <<"\nPhone: "<< "\nEmail: "<< email << "\n";
+void Contact::display() const {
+    std::cout << "Name: " << name << "\nPhone: " << phone << "\nEmail: " << email << "\n";
 }
 
 nlohmann::json Contact::toJson() const {
-  return nlohmann::json{{"name", name}, {"phone", phone}, {"email", email}};
+    return nlohmann::json{{"name", name}, {"phone", phone}, {"email", email}};
 }
 
 Contact Contact::fromJson(const nlohmann::json& j) {
-  return Contact(j.at("name"), j.at("phone"), j.at("email"));
+    return Contact(j.at("name"), j.at("phone"), j.at("email"));
 }
+
